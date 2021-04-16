062542fe0301e5392a6985114aabdd790c6c45db
Evandro Capovilla <evandrocapovillajr@gmail.com>
2021-04-16 17:44:24 -0300

    lint updated

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 const express = require('express');
 const consign = require('consign');
-const sequelize = require('./config/database');
 const swaggerUi = require('swagger-ui-express');
+const sequelize = require('./config/database');
 const swaggerDocument = require('./swagger_output.json');
 
 const start = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
-    try {
-      await sequelize.authenticate();
-      console.log('Connection has been established successfully.');
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-    }
-    
-    try {
-      const app = express();
+  try {
+    const app = express();
 
-      app.use(express.json());
-      app.use(express.urlencoded({
-          extended: true
-        }));
-      app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-  
-      consign()
+    app.use(express.json());
+    app.use(express.urlencoded({
+      extended: true,
+    }));
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+    consign()
       .include('adapters/api/routes')
       .into(app);
 
-      app.listen(3000, () => console.log('listening port 3000'));
-    } catch (err) {
-      console.log(err);
-      process.exit(1)
-    }
+    app.listen(3000, () => console.log('listening port 3000'));
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
   }
-  start()
+};
+start();
--- swagger.js
+++ swagger.js
@@ -1,6 +1,6 @@
-const swaggerAutogen = require('swagger-autogen')()
+const swaggerAutogen = require('swagger-autogen')();
 
-const outputFile = './swagger_output.json'
-const endpointsFiles = ['./adapters/api/routes/ong.js']
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['./adapters/api/routes/ong.js'];
 
-swaggerAutogen(outputFile, endpointsFiles)
+swaggerAutogen(outputFile, endpointsFiles);
